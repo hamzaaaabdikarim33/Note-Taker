5963bff96c697ba141858b7959d16ebaf9a8cdef
hamzaaaabdikarim33 <55111034+hamzaaaabdikarim33@users.noreply.github.com>
2020-03-13 00:07:33 -0500

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Note-Taker
+Create an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+
+![alt](Image/notetaker.png)
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+var router =require("express").Router();
+var path =require("path");
+
+
+router.get("index", function(req, res){
+  res.sendFile(path.join(__dirname,"../public/index.html"));
+});
+router.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/notes.html"));
+});
+
+
+module.exports = router
